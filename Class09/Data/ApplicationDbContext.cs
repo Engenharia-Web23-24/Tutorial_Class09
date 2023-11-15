d74d1df5fdbcf36f1ac29165f413b60878b40ce4
lfbutad <lfb@utad.pt>
2023-11-15 18:32:52 +0000

    Resolução tutorial aula 9

---
+++ Class09/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Class09.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Class09.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Perfil> Perfils { get; set; }
+    }
+}
